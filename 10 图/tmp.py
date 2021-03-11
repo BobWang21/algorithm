b9a6ada53cf279a77f8593a5acfee6131990972a
bobwangbao <bobwangbao@didiglobal.com>
2021-03-11 18:54:55 +0800

    fix

--- 10 图/tmp.py
+++ 10 图/tmp.py
@@ -96,12 +96,12 @@ def criticalConnections(n, connections):
     return res
 
 
-def bipartite_graphs(left2right):
-    right2left = dict()
-    for left, adj in left2right.items():
+def bipartite_graphs(l2r):
+    r2l = dict()
+    for left, adj in l2r.items():
         for right, score in adj.items():
-            right2left.setdefault(right, dict())
-            right2left[right][left] = score
+            r2l.setdefault(right, dict())
+            r2l[right][left] = score
 
     visit_left = dict()
     visit_right = dict()
@@ -111,23 +111,48 @@ def bipartite_graphs(left2right):
         if visit_left.get(i, False):
             return
         visit_left[i] = True
-        path[i] = left2right[i]
-        for j in left2right[i]:
+        path[i] = l2r[i]
+        for j in l2r[i]:
             if visit_right.get(i, False):
                 continue
             visit_right[j] = True
-            for k in right2left[j]:
+            for k in r2l[j]:
                 helper(k, path)
 
         return
 
-    for i in left2right:
+    for i in l2r:
         if visit_left.get(i, False):
             continue
         path = dict()
         helper(i, path)
         res.append(path)
 
+    return res
+
+
+def hungarian(graph: object, m, n):
+    match = [False] * n
+    visit_y = [False] * n
+
+    def helper(i):
+        # 遍历y顶点
+        for j, val in graph[i].items():
+            # 每一轮匹配, 每个y顶点只尝试一次
+            if visit_y[j]:
+                continue
+            visit_y[j] = True
+            if match[j] == -1 or helper(match[j]):
+                match[j] = i
+                return True
+
+        return True
+
+    res = 0
+    for i in range(m):
+        visit_y = [False] * n
+        if helper(i):
+            res += 1
     return res
 
 
@@ -144,3 +169,10 @@ if __name__ == '__main__':
         4: {3: 0},
     }
     print(bipartite_graphs(left2right))
+
+    graph = {
+        0: {0: 0, 2: 0},
+        1: {0: 0, 1: 0},
+        2: {1: 0}
+    }
+    print(hungarian(graph, 3, 3))
